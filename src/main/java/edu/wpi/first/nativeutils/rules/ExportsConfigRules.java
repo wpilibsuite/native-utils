a4865c8c8ebcc84ec5537560fc37c4385ed727c9
Thad House <thadhouse1@gmail.com>
2018-04-10 21:11:46 -0700

    Finally Fixes file creation

--- src/main/java/edu/wpi/first/nativeutils/rules/ExportsConfigRules.java
+++ src/main/java/edu/wpi/first/nativeutils/rules/ExportsConfigRules.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.gradle.api.Project;
 import org.gradle.api.Task;
@@ -62,89 +63,89 @@ public class ExportsConfigRules extends RuleSource {
               if (binary.getTargetPlatform().getOperatingSystem().isWindows() &&
                   binary instanceof SharedLibraryBinarySpec &&
                   !excludeBuildTypes.contains(binary.getBuildType().getName())) {
-                    SharedLibraryBinarySpec sBinary = (SharedLibraryBinarySpec)binary;
+                SharedLibraryBinarySpec sBinary = (SharedLibraryBinarySpec)binary;
 
-                    String exportsTaskName = "generateExports" + binary.getBuildTask().getName();
-                    File tmpDir = project.file(project.getBuildDir() + "/tmp/" + exportsTaskName);
-                    File defFile = project.file(tmpDir.toString() + "/exports.def");
+                String exportsTaskName = "generateExports" + binary.getBuildTask().getName();
 
-                    Task exportsTask = project.getTasks().create(exportsTaskName, ExportsGenerationTask.class, task -> {
-                      task.getInputs().files(((AbstractLinkTask)sBinary.getTasks().getLink()).getSource());
+                Task exportsTask = project.getTasks().create(exportsTaskName, ExportsGenerationTask.class, task -> {
+                  task.getInputs().files(((AbstractLinkTask)sBinary.getTasks().getLink()).getSource());
+                  File tmpDir = project.file(project.getBuildDir() + "/tmp/" + exportsTaskName);
+                  File defFile = project.file(tmpDir.toString() + "/exports.def");
+                  sBinary.getLinker().args("/DEF:" + defFile.toString());
+                  sBinary.getTasks().getLink().getInputs().file(defFile);
+                  task.getOutputs().file(defFile);
 
-                      task.getOutputs().file(defFile);
+                  task.doLast(last -> {
+                    tmpDir.mkdirs();
+                    String exeName = ExportsStorage.getGeneratorFilePath();
+                    project.exec(exec -> {
+                      exec.setExecutable(exeName);
+                      exec.args(defFile);
+                      exec.args(((AbstractLinkTask)sBinary.getTasks().getLink()).getSource());
+                    });
 
-                      task.doLast(last -> {
-                        tmpDir.mkdirs();
-                        String exeName = ExportsStorage.getGeneratorFilePath();
-                        project.exec(exec -> {
-                          exec.setExecutable(exeName);
-                          exec.args(defFile);
-                          exec.args(((AbstractLinkTask)sBinary.getTasks().getLink()).getSource());
-                        });
+                    List<String> lines = new ArrayList<>();
+                    List<String> excludeSymbols;
+                    boolean isX86 = sBinary.getTargetPlatform().getArchitecture().getName().equals("x86");
+                    if (isX86) {
+                      excludeSymbols = config.getX86ExcludeSymbols();
+                    } else {
+                      excludeSymbols = config.getX64ExcludeSymbols();
+                    }
 
-                        List<String> lines = new ArrayList<>();
-                        List<String> excludeSymbols;
-                        boolean isX86 = sBinary.getTargetPlatform().getArchitecture().getName().equals("x86");
-                        if (isX86) {
-                          excludeSymbols = config.getX86ExcludeSymbols();
-                        } else {
-                          excludeSymbols = config.getX64ExcludeSymbols();
+                    if (excludeSymbols == null) {
+                      excludeSymbols = new ArrayList<>();
+                    }
+                    final List<String> exSymbols = excludeSymbols;
+                    try (Stream<String> stream = Files.lines(defFile.toPath())) {
+                      stream.map(s -> s.trim()).forEach(line -> {
+                        String symbol = line;
+                        int space = line.indexOf(' ');
+                        if (space != -1) {
+                          symbol = symbol.substring(0, space);
                         }
-
-                        if (excludeSymbols == null) {
-                          excludeSymbols = new ArrayList<>();
-                        }
-                        final List<String> exSymbols = excludeSymbols;
-                        try {
-                          Files.lines(defFile.toPath()).map(s -> s.trim()).forEach(line -> {
-                            String symbol = line;
-                            int space = line.indexOf(' ');
-                            if (space != -1) {
-                              symbol = symbol.substring(0, space);
-                            }
-                            if (!symbol.equals("EXPORTS") && !exSymbols.contains(symbol)) {
-                              lines.add(symbol);
-                            }
-                          });
-                        } catch (IOException ex) {
-
-                        }
-
-                        if (isX86) {
-                          Closure symbolFilter = config.getX86SymbolFilter();
-                          if (symbolFilter != null) {
-                            symbolFilter.call(lines);
-                          }
-                        } else {
-                          Closure symbolFilter = config.getX64SymbolFilter();
-                          if (symbolFilter != null) {
-                            symbolFilter.call(lines);
-                          }
-                        }
-
-                        try (BufferedWriter writer = Files.newBufferedWriter(defFile.toPath()))
-                        {
-                            writer.append("EXPORTS").append('\n');
-                            for (String line : lines) {
-                              writer.append(line).append('\n');
-                            }
-                        } catch (IOException ex) {
+                        if (!symbol.equals("EXPORTS") && !exSymbols.contains(symbol)) {
+                          lines.add(symbol);
                         }
                       });
-                    });
+                    } catch (IOException ex) {
 
-                    sBinary.getTasks().withType(AbstractNativeSourceCompileTask.class, it -> {
-                      exportsTask.dependsOn(it);
-                    });
-
-                    Task linkTask = sBinary.getTasks().getLink();
-                    sBinary.getLinker().args("/DEF:" + defFile.toString());
-                    for (Object o : linkTask.getDependsOn()) {
-                      exportsTask.dependsOn(o);
                     }
-                    linkTask.dependsOn(exportsTask);
-                    linkTask.getInputs().file(defFile);
-                  }
+
+                    if (isX86) {
+                      Closure symbolFilter = config.getX86SymbolFilter();
+                      if (symbolFilter != null) {
+                        symbolFilter.call(lines);
+                      }
+                    } else {
+                      Closure symbolFilter = config.getX64SymbolFilter();
+                      if (symbolFilter != null) {
+                        symbolFilter.call(lines);
+                      }
+                    }
+
+                    try (BufferedWriter writer = Files.newBufferedWriter(defFile.toPath()))
+                    {
+                        writer.append("EXPORTS").append('\n');
+                        for (String line : lines) {
+                          writer.append(line).append('\n');
+                        }
+                    } catch (IOException ex) {
+                    }
+                  });
+                });
+
+                sBinary.getTasks().withType(AbstractNativeSourceCompileTask.class, it -> {
+                  exportsTask.dependsOn(it);
+                });
+
+                Task linkTask = sBinary.getTasks().getLink();
+
+                for (Object o : linkTask.getDependsOn()) {
+                  exportsTask.dependsOn(o);
+                }
+                linkTask.dependsOn(exportsTask);
+              }
             }
           }
         }
