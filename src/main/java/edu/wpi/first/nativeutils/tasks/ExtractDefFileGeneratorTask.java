b42971f9cf4e0f44dcd49071229798ff58d93325
Thad House <thadhouse1@gmail.com>
2018-07-21 00:34:51 -0700

    Fix appveyor warnings

--- src/main/java/edu/wpi/first/nativeutils/tasks/ExtractDefFileGeneratorTask.java
+++ src/main/java/edu/wpi/first/nativeutils/tasks/ExtractDefFileGeneratorTask.java
@@ -1,11 +1,10 @@
 package edu.wpi.first.nativeutils.tasks;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.Task;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.OutputFile;
 
-public class ExtractDefFileGeneratorTask extends DefaultTask implements Task {
+public class ExtractDefFileGeneratorTask extends DefaultTask {
   @OutputFile
   public RegularFileProperty defFileGenerator = newOutputFile();
 }
