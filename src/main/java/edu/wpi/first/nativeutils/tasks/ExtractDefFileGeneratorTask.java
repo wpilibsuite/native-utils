b4bdfdb4dfd7b9e720af3e6bdff8cf61b2d00ccb
Thad House <thadhouse1@gmail.com>
2018-12-02 19:57:58 -0800

    Remove deprecation warning

--- src/main/java/edu/wpi/first/nativeutils/tasks/ExtractDefFileGeneratorTask.java
+++ src/main/java/edu/wpi/first/nativeutils/tasks/ExtractDefFileGeneratorTask.java
@@ -1,10 +1,18 @@
 package edu.wpi.first.nativeutils.tasks;
 
+import javax.inject.Inject;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.tasks.OutputFile;
 
 public class ExtractDefFileGeneratorTask extends DefaultTask {
   @OutputFile
-  public RegularFileProperty defFileGenerator = newOutputFile();
+  public RegularFileProperty defFileGenerator;
+
+  @Inject
+  public ExtractDefFileGeneratorTask(ObjectFactory factory) {
+    defFileGenerator = factory.fileProperty();
+  }
 }
