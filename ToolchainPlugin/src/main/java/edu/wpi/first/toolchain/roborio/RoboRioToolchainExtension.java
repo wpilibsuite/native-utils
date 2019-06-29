5c23e63904c10cc0b7709303d69e0c85fba6766d
Thad House <thadhouse1@gmail.com>
2019-06-28 21:01:34 -0700

    Fix rio gcc to 7.3

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
@@ -2,9 +2,9 @@ package edu.wpi.first.toolchain.roborio;
 
 public class RoboRioToolchainExtension {
 
-    public String versionLow = "7.4";
-    public String versionHigh = "7.4";
-    public String toolchainVersion = "2020-7.4.0";
+    public String versionLow = "7.3";
+    public String versionHigh = "7.3";
+    public String toolchainVersion = "2020-7.3.0";
     public String toolchainTag = "v2020-1";
 
     public String year = "2020";
