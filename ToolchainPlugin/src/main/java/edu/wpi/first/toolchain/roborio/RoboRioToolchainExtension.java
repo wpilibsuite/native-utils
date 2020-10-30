bd59530ecdcddb3be0fed0606635d1c5a2ad5fd4
Thad House <thadhouse1@gmail.com>
2020-10-29 17:04:07 -0700

    Update to 2021 toolchain

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
@@ -4,9 +4,9 @@ public class RoboRioToolchainExtension {
 
     public String versionLow = "7.3";
     public String versionHigh = "7.3";
-    public String toolchainVersion = "2020-7.3.0";
-    public String toolchainTag = "v2020-3";
+    public String toolchainVersion = "2021-7.3.0";
+    public String toolchainTag = "v2021-2";
 
-    public String year = "2020";
+    public String year = "2021";
 
 }
