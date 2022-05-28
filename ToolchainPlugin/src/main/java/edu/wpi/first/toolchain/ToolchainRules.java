eea3ec7dc212e2ba30d2765464cf1c632a6048be
Thad House <ThadHouse@users.noreply.github.com>
2022-05-28 09:21:49 -0700

    Remove 32 bit windows platform from being added as a default platform (#102)
    * Remove 32 bit windows platform from being added as a default platform
    
    * Patch

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/ToolchainRules.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/ToolchainRules.java
@@ -104,11 +104,6 @@ public class ToolchainRules extends RuleSource {
             NativePlatform desktop = platforms.maybeCreate(NativePlatforms.desktop, NativePlatform.class);
             desktop.architecture(NativePlatforms.desktopArch().replaceAll("-", "_"));
 
-            if (OperatingSystem.current().isWindows() && NativePlatforms.desktopArch().equals("x86-64")) {
-                NativePlatform win32 = platforms.maybeCreate("windowsx86", NativePlatform.class);
-                win32.architecture("x86");
-            }
-
             for (CrossCompilerConfiguration config : ext.getCrossCompilers()) {
                 NativePlatform configedPlatform = platforms.maybeCreate(config.getName(), NativePlatform.class);
                 configedPlatform.architecture(config.getArchitecture());
