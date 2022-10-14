356e3eb19004adf9bcdb877059fbaae68b315d97
Peter Johnson <johnson.peter@gmail.com>
2022-10-14 16:04:35 -0700

    Update toolchains to v2023-3 tag (#119)

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/arm32/Arm32ToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/arm32/Arm32ToolchainExtension.java
@@ -5,6 +5,6 @@ public class Arm32ToolchainExtension {
     public String versionLow = "10.2.0";
     public String versionHigh = "10.2.0";
     public String toolchainVersion = "2023-10.2.0";
-    public String toolchainTag = "v2023-1";
+    public String toolchainTag = "v2023-3";
 
 }
--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
@@ -5,7 +5,7 @@ public class RoboRioToolchainExtension {
     public String versionLow = "12.1.0";
     public String versionHigh = "12.1.0";
     public String toolchainVersion = "2023-12.1.0";
-    public String toolchainTag = "v2023-1";
+    public String toolchainTag = "v2023-3";
 
     public String year = "2023";
 
