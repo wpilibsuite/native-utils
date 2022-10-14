b8730597f52684215a4468ce677b66aa707918a0
Thad House <thadhouse1@gmail.com>
2022-10-13 17:46:44 -0700

    Update roborio toolchain to 2023

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/roborio/RoboRioToolchainExtension.java
@@ -2,11 +2,11 @@ package edu.wpi.first.toolchain.roborio;
 
 public class RoboRioToolchainExtension {
 
-    public String versionLow = "7.3";
-    public String versionHigh = "7.3";
-    public String toolchainVersion = "2022-7.3.0";
-    public String toolchainTag = "v2022-1";
+    public String versionLow = "12.1.0";
+    public String versionHigh = "12.1.0";
+    public String toolchainVersion = "2023-12.1.0";
+    public String toolchainTag = "v2023-1";
 
-    public String year = "2022";
+    public String year = "2023";
 
 }
