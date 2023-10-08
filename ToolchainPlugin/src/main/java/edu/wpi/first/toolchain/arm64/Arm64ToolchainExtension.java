81e843600996e9ec8c25db6638ecd4e03e4bdd2b
Peter Johnson <johnson.peter@gmail.com>
2023-10-08 14:53:36 -0700

    Update remainder of toolchains

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/arm64/Arm64ToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/arm64/Arm64ToolchainExtension.java
@@ -5,7 +5,7 @@ import javax.inject.Inject;
 import edu.wpi.first.toolchain.opensdk.OpenSdkToolchainExtension;
 
 public abstract class Arm64ToolchainExtension extends OpenSdkToolchainExtension {
-    public static final String TOOLCHAIN_VERSION = "2023-10.2.0";
+    public static final String TOOLCHAIN_VERSION = "2024-10.2.0";
     public static final String INSTALL_SUBDIR = "arm64";
 
     @Inject
