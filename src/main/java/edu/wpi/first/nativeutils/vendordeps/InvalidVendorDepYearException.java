9651366cb17ba4eae5e8a0fabfbb1638c8c323ed
Thad House <thadhouse1@gmail.com>
2023-11-12 11:31:43 -1000

    Make the warning on out of year vendor deps more serious

--- src/main/java/edu/wpi/first/nativeutils/vendordeps/InvalidVendorDepYearException.java
+++ src/main/java/edu/wpi/first/nativeutils/vendordeps/InvalidVendorDepYearException.java
@@ -4,7 +4,7 @@ import edu.wpi.first.nativeutils.vendordeps.WPIVendorDepsExtension.JsonDependenc
 
 public class InvalidVendorDepYearException extends RuntimeException {
     public InvalidVendorDepYearException(JsonDependency dependency, String requiredYear) {
-        super(String.format("Vendor Dependency %s has invalid year %s. Expected to be %s", dependency.name,
+        super(String.format("Vendor Dependency %s has invalid year %s. Expected to be %s. Do not manually change a prior year dependency. This will not work, and the WPILib team will not offer support if you attempt this.", dependency.name,
                 dependency.frcYear, requiredYear));
     }
 }
