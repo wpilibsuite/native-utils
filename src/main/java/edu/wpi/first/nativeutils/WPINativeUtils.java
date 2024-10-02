718fd0e6913afb7b877478b79755903c61b5f9fe
Jade Turner <spacey-sooty@proton.me>
2024-10-02 13:28:34 +0800

    option magic
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/edu/wpi/first/nativeutils/WPINativeUtils.java
+++ src/main/java/edu/wpi/first/nativeutils/WPINativeUtils.java
@@ -2,10 +2,22 @@ package edu.wpi.first.nativeutils;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.options.Option;
 import org.gradle.nativeplatform.tasks.AbstractLinkTask;
 import org.gradle.nativeplatform.tasks.LinkExecutable;
 
 public class WPINativeUtils implements Plugin<Project> {
+  private String developerID = null;
+
+  @Option(option = "sign", description = "Sign with developer ID (MacOS only)")
+  public void sign(String developerID) {
+    if (!System.getProperty("os.name").startsWith("Mac")) {
+      throw new RuntimeException("Can't sign binaries on non mac platforms");
+    }
+
+    this.developerID = developerID;
+  }
+
   @Override
   public void apply(Project project) {
     project.getPluginManager().apply(NativeUtils.class);
@@ -17,7 +29,7 @@ public class WPINativeUtils implements Plugin<Project> {
     project.getPluginManager().apply(RpathRules.class);
 
     if (System.getProperty("os.name").startsWith("Mac")) {
-      if (project.hasProperty("developerID")) {
+      if (developerID != null) {
         project.getTasks().withType(AbstractLinkTask.class).forEach((task) -> {
               // Don't sign any executables because codesign complains
               // about relative rpath.
@@ -26,7 +38,7 @@ public class WPINativeUtils implements Plugin<Project> {
                 String path = task.getLinkedFile().getAsFile().get().getAbsolutePath();
                 ProcessBuilder builder = new ProcessBuilder();
                 var codesigncommand = String.format("codesign --force --strict --timestamp --options=runtime "
-                + "--version -s %s %s", project.findProperty("developerID"), path);
+                + "--version -s %s %s", developerID, path);
                 builder.command("sh", "-c", codesigncommand);
                 builder.directory(project.getRootDir());
               }
