012e31c5694fe6c0c7007b4e989732f583b3348d
Jade Turner <spacey-sooty@proton.me>
2024-10-02 12:44:22 +0800

    Add support for signing binaries
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/edu/wpi/first/nativeutils/WPINativeUtils.java
+++ src/main/java/edu/wpi/first/nativeutils/WPINativeUtils.java
@@ -2,6 +2,8 @@ package edu.wpi.first.nativeutils;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.nativeplatform.tasks.AbstractLinkTask;
+import org.gradle.nativeplatform.tasks.LinkExecutable;
 
 public class WPINativeUtils implements Plugin<Project> {
   @Override
@@ -13,5 +15,21 @@ public class WPINativeUtils implements Plugin<Project> {
     nativeExt.addWpiExtension();
 
     project.getPluginManager().apply(RpathRules.class);
-  }
+
+    if (project.hasProperty("developerID")) {
+      project.getTasks().withType(AbstractLinkTask.class).forEach((task) -> {
+            // Don't sign any executables because codesign complains
+            // about relative rpath.
+            if (!(task instanceof LinkExecutable)) {
+              // Get path to binary.
+              String path = task.getLinkedFile().getAsFile().get().getAbsolutePath();
+              ProcessBuilder builder = new ProcessBuilder();
+              var codesigncommand = String.format("codesign --force --strict --timestamp --options=runtime "
+              + "--version -s %s %s", project.findProperty("developerID"), path);
+              builder.command("sh", "-c", codesigncommand);
+              builder.directory(project.getRootDir());
+            }
+        });
+      }
+    }
 }
