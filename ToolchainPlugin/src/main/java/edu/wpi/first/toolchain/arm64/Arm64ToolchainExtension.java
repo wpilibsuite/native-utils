e487f8a43529bc51d6c93555e09ecaffb7ae5f8e
Thad House <ThadHouse@users.noreply.github.com>
2024-12-06 13:10:50 -0800

    Update arm toolchains to bookworm (#230)

--- ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/arm64/Arm64ToolchainExtension.java
+++ ToolchainPlugin/src/main/java/edu/wpi/first/toolchain/arm64/Arm64ToolchainExtension.java
@@ -5,14 +5,14 @@ import javax.inject.Inject;
 import edu.wpi.first.toolchain.opensdk.OpenSdkToolchainExtension;
 
 public abstract class Arm64ToolchainExtension extends OpenSdkToolchainExtension {
-    public static final String TOOLCHAIN_VERSION = "2025-10.2.0";
+    public static final String TOOLCHAIN_VERSION = "2025-12.2.0";
     public static final String INSTALL_SUBDIR = "arm64";
 
     @Inject
     public Arm64ToolchainExtension() {
         super();
-        getVersionLow().convention("10.2.0");
-        getVersionHigh().convention("10.2.0");
+        getVersionLow().convention("12.2.0");
+        getVersionHigh().convention("12.2.0");
         getToolchainVersion().convention(TOOLCHAIN_VERSION);
     }
 }
