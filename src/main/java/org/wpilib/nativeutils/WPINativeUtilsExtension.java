bc5e1b83d2735c1c2ef00fed6ccb1ff3e76ccb8a
sciencewhiz <sciencewhiz@users.noreply.github.com>
2026-08-23 17:07:23 -0700

    Add field library to library lists
    2027.14.2 has not been tagged, so not bumping version

--- src/main/java/org/wpilib/nativeutils/WPINativeUtilsExtension.java
+++ src/main/java/org/wpilib/nativeutils/WPINativeUtilsExtension.java
@@ -479,13 +479,13 @@ public class WPINativeUtilsExtension {
 
         configs.register("wpilib_static", AllPlatformsCombinedNativeDependency.class, c -> {
             ListProperty<String> d = c.getDependencies();
-            d.set(List.of("wpilib_drivers_static", "wpilibc_static", "ntcore_static", "hal_static", "datalog_static", "wpimath_static",
+            d.set(List.of("wpilib_drivers_static", "wpilibc_static", "ntcore_static", "hal_static", "datalog_static", "fields_static", "wpimath_static",
                     "wpinet_static", "telemetry_static", "tunables_static", "wpiutil_static", "mrclib_shared"));
         });
 
         configs.register("wpilib_shared", AllPlatformsCombinedNativeDependency.class, c -> {
             ListProperty<String> d = c.getDependencies();
-            d.set(List.of("wpilib_drivers_shared","wpilibc_shared", "ntcore_shared", "hal_shared", "datalog_shared", "wpimath_shared",
+            d.set(List.of("wpilib_drivers_shared","wpilibc_shared", "ntcore_shared", "hal_shared", "datalog_shared", "fields_shared", "wpimath_shared",
                     "wpinet_shared", "telemetry_shared", "tunables_shared", "wpiutil_shared", "mrclib_shared"));
         });
 
