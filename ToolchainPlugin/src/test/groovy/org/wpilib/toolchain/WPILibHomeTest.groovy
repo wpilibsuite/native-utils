8ce3fe542b874b9fd8f7e74033b1543de9fc6f07
Thad House <thadhouse1@gmail.com>
2026-08-29 21:02:52 -0700

    Fix test

--- ToolchainPlugin/src/test/groovy/org/wpilib/toolchain/WPILibHomeTest.groovy
+++ ToolchainPlugin/src/test/groovy/org/wpilib/toolchain/WPILibHomeTest.groovy
@@ -11,7 +11,7 @@ class WPILibHomeTest extends Specification {
         isWindows | isMacOsX | isLinux | userHome       | publicFolder       | xdgDataHome        || expected
         true      | false    | false   | "/home/user"   | "C:\\Users\\Team"  | null               || new File("C:\\Users\\Team", "wpilib")
         true      | false    | false   | "/home/user"   | null               | null               || new File("C:\\Users\\Public", "wpilib")
-        false     | true     | false   | "/Users/team"  | null               | null               || new File("/Users/team", ".wpilib")
+        false     | true     | false   | "/Users/team"  | null               | null               || new File("/Users/team", "wpilib")
         false     | false    | true    | "/home/team"   | null               | "/data/team/share" || new File("/data/team/share", "wpilib")
         false     | false    | true    | "/home/team"   | null               | null               || new File("/home/team/.local/share", "wpilib")
         false     | false    | true    | "/home/team"   | null               | ""                 || new File("/home/team/.local/share", "wpilib")
